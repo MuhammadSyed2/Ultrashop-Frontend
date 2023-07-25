dd304f9a34612389af7ce7a1dfa99e9c1868b872
sobhanifahim <57230287+sobhanifahim@users.noreply.github.com>
2023-07-25 11:53:42 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Loginform from './components/login';
+import {BrowserRouter,Route,Routes} from "react-router-dom";
+import Registration from './components/registration';
+import Homepage from './components/Homepage';
+import Profile from './components/profile';
+import Forgotpassword from './components/forgotpassword.js';
+import MerchantDash from './components/merchantDashboard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <BrowserRouter>     
+        <Routes>
+                <Route exact path = "/" element={<Loginform/>}></Route>
+                <Route path = "/register" element={<Registration/>}></Route>
+                <Route path = "/homepage" element={<Homepage/>}></Route>
+                <Route path = "/profile" element={<Profile/>}></Route>
+                <Route path = "/forgotpassword" element={<Forgotpassword/>}></Route>
+                <Route path = "/merchantdashboard" element={<MerchantDash/>}></Route>
+        </Routes>
+        </BrowserRouter>
+                
     </div>
   );
 }
